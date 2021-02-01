b926aeb8592bd1b6de595d7d23c2e9e9f9fc0430
Eduardo Viegas <engenedu@hotmail.com>
2021-02-01 18:21:45 -0300

    finalizando a activity resultados

--- app/src/main/java/com/innaval/odontoapp/MainActivity.kt
+++ app/src/main/java/com/innaval/odontoapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.innaval.odontoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ivLogo: ImageView
+    lateinit var tvMegaApp: TextView
+    lateinit var tvFrase: TextView
+    lateinit var btResultado: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btResultado = findViewById(R.id.bt_mostrar_resultado)
+
+        btResultado.setOnClickListener{
+
+            val abrirResultado = Intent(this, ResultadosActivity::class.java)
+        startActivity(abrirResultado)
+        }
     }
 }
